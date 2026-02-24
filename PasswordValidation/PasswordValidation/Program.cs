7c95bdadec6542dcd120b3613d555f2a1d81dbc7
Nitin Singh <nitinsingh7325@gmail.com>
2026-02-24 08:04:21 +0530

    Add files via upload

---
+++ PasswordValidation/PasswordValidation/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PasswordValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            bool isValid = false;
+
+            while (!isValid)
+            {
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+
+                bool hasUpper = false;
+                bool hasLower = false;
+                bool hasDigit = false;
+                bool hasSpecial = false;
+
+                if (password.Length < 8)
+                {
+                    Console.WriteLine("Password must be at least 8 characters long.\n");
+                    continue;
+                }
+
+                for (int i = 0; i < password.Length; i++)
+                {
+                    char ch = password[i];
+
+                    if (ch >= 'A' && ch <= 'Z')
+                        hasUpper = true;
+                    else if (ch >= 'a' && ch <= 'z')
+                        hasLower = true;
+                    else if (ch >= '0' && ch <= '9')
+                        hasDigit = true;
+                    else
+                        hasSpecial = true;
+                }
+
+                if (hasUpper && hasLower && hasDigit && hasSpecial)
+                {
+                    isValid = true;
+                    Console.WriteLine("\n Password is valid!");
+                }
+                else
+                {
+                    Console.WriteLine("\n Password must contain:");
+                    if (!hasUpper) Console.WriteLine("- At least one uppercase letter");
+                    if (!hasLower) Console.WriteLine("- At least one lowercase letter");
+                    if (!hasDigit) Console.WriteLine("- At least one digit");
+                    if (!hasSpecial) Console.WriteLine("- At least one special character");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Prime/Prime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isPrime = true;
+
+            Console.WriteLine("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n <= 1)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i <= n / 2; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime)
+                Console.WriteLine(n + " is a Prime Number");
+            else
+                Console.WriteLine(n + " is not a Prime Number");
+        }
+    }
+}
