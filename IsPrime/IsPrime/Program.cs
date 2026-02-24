eccded1f7b6d9f7d45cde1ff60f9f55b618d9046
Nitin Singh <nitinsingh7325@gmail.com>
2026-02-24 08:01:38 +0530

    Add files via upload

---
+++ IsPrime/IsPrime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main()
+        {
+            int start, end;
+
+            Console.Write("Enter starting number: ");
+            start = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter ending number: ");
+            end = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Prime numbers between " + start + " and " + end + ":");
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i <= 1)
+                    continue;
+
+                bool isPrime = true;
+
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                    Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
---
+++ MinimumNumber/MinimumNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MinimumNumber
+{
+    class MinimumOfThree
+    {
+        static void Main()
+        {
+            int a, b, c;
+
+            Console.Write("Enter first number: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter third number: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a <= b && a <= c)
+            {
+                Console.WriteLine("Minimum number is: " + a);
+            }
+            else if (b <= a && b <= c)
+            {
+                Console.WriteLine("Minimum number is: " + b);
+            }
+            else
+            {
+                Console.WriteLine("Minimum number is: " + c);
+            }
+        }
+    }
+}
