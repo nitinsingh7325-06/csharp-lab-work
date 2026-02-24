3841e6bd575ca13d2da9052162d0c7188c309760
Nitin Singh <nitinsingh7325@gmail.com>
2026-02-24 07:59:04 +0530

    Add files via upload

---
+++ CompoundInterest/CompoundInterest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CompoundInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p = double.Parse(Console.ReadLine());
+            double r = double.Parse(Console.ReadLine());
+            double n = double.Parse(Console.ReadLine());
+            double ci = p* Math.Pow((1 + r / 100), n) -p;
+            Console.WriteLine("Principal Amount is :" + (p));
+            Console.WriteLine("Rate of interest is :" + (r));
+            Console.WriteLine("Numbers of years is :" + (n));
+            Console.WriteLine("Compound Interest is :" + (ci));
+        }
+    }
+}
