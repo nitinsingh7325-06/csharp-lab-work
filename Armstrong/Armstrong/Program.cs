4059f852ef1e08247513533f66642ad11b0fa372
Nitin Singh <nitinsingh7325@gmail.com>
2026-02-24 07:58:27 +0530

    Add files via upload

---
+++ Armstrong/Armstrong/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num, temp, sum = 0;
+
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+
+            temp = num;
+
+            while (num > 0)
+            {
+                int rem = num % 10;
+                sum = sum + (rem * rem * rem);
+                num = num / 10;
+            }
+
+            if (temp == sum)
+                Console.WriteLine("Number is Armstrong");
+            else
+                Console.WriteLine("Number is Not Armstrong");
+        }
+    }
+}
